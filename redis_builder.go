20a9fb14e2d06f349b48d430ab5680adf5ba0cb1
Matthew M. Keeler <mkeeler@launchdarkly.com>
2026-08-20 10:36:44 -0400

    feat!: Split StoreBuilder into DataStoreBuilder and BigSegmentStoreBuilder (#55)

--- redis_builder.go
+++ redis_builder.go
@@ -10,11 +10,68 @@ import (
 )
 
 const (
-	// DefaultURL is the default value for StoreBuilder.URL.
+	// DefaultURL is the default value for the URL option of either builder.
 	DefaultURL = "redis://localhost:6379"
-	// DefaultPrefix is the default value for StoreBuilder.Prefix.
+	// DefaultPrefix is the default value for the Prefix option of either builder.
 	DefaultPrefix = "launchdarkly"
 )
+
+// builderOptions holds the Redis connection settings that both builders accept. The setters are
+// shared by both builders so that each option behaves identically no matter which builder it was
+// set on.
+type builderOptions struct {
+	prefix      string
+	pool        Pool
+	url         string
+	dialOptions []r.DialOption
+}
+
+func defaultBuilderOptions() builderOptions {
+	return builderOptions{
+		prefix: DefaultPrefix,
+		url:    DefaultURL,
+	}
+}
+
+func (o *builderOptions) setPrefix(prefix string) {
+	if prefix == "" {
+		prefix = DefaultPrefix
+	}
+	o.prefix = prefix
+}
+
+func (o *builderOptions) setURL(url string) {
+	if url == "" {
+		url = DefaultURL
+	}
+	o.url = url
+}
+
+func (o *builderOptions) setHostAndPort(host string, port int) {
+	o.setURL(fmt.Sprintf("redis://%s:%d", host, port))
+}
+
+func (o *builderOptions) setPool(pool Pool) {
+	o.pool = pool
+}
+
+func (o *builderOptions) setDialOptions(options []r.DialOption) {
+	o.dialOptions = options
+}
+
+// DataStoreBuilder is a builder for configuring the Redis-based persistent data store.
+//
+// Obtain an instance of this type by calling [DataStore]. After calling its methods to specify any
+// non-default Redis options you may want, pass it to
+// [github.com/launchdarkly/go-server-sdk/v7/ldcomponents.PersistentDataStore] and store the result
+// in the SDK configuration's DataStore field.
+//
+// To configure a store for Big Segments instead, use [BigSegmentStore]. The two kinds of store are
+// independent of each other. You do not need a Big Segment store if you are not using the Big
+// Segments feature, and you do not need to use the same database for both.
+type DataStoreBuilder struct {
+	opts builderOptions
+}
 
 // DataStore returns a configurable builder for a Redis-backed persistent data store.
 //
@@ -36,14 +93,92 @@ const (
 //	config.DataStore = ldcomponents.PersistentDataStore(
 //		ldredis.DataStore().HostAndPort("host1", 6379),
 //	).CacheSeconds(15)
-func DataStore() *StoreBuilder[subsystems.PersistentDataStore] {
-	return &StoreBuilder[subsystems.PersistentDataStore]{
-		builderOptions: builderOptions{
-			prefix: DefaultPrefix,
-			url:    DefaultURL,
-		},
-		factory: createPersistentDataStore,
-	}
+func DataStore() *DataStoreBuilder {
+	return &DataStoreBuilder{opts: defaultBuilderOptions()}
+}
+
+// Prefix specifies a string that should be prepended to all Redis keys used by the data store.
+// A colon will be added to this automatically. If this is unspecified or empty, [DefaultPrefix] will be used.
+func (b *DataStoreBuilder) Prefix(prefix string) *DataStoreBuilder {
+	b.opts.setPrefix(prefix)
+	return b
+}
+
+// URL specifies the Redis host URL. If not specified, the default value is [DefaultURL].
+//
+// Note that some Redis client features can also be specified as part of the URL: Redigo supports
+// the redis:// syntax (https://www.iana.org/assignments/uri-schemes/prov/redis), which can include a
+// password and a database number, as well as rediss://
+// (https://www.iana.org/assignments/uri-schemes/prov/rediss), which enables TLS.
+func (b *DataStoreBuilder) URL(url string) *DataStoreBuilder {
+	b.opts.setURL(url)
+	return b
+}
+
+// HostAndPort is a shortcut for specifying the Redis host address as a hostname and port.
+func (b *DataStoreBuilder) HostAndPort(host string, port int) *DataStoreBuilder {
+	b.opts.setHostAndPort(host, port)
+	return b
+}
+
+// Pool specifies that the data store should use a specific connection pool configuration. If not
+// specified, it will create a default configuration (see package description). Specifying this
+// option will cause any address specified with URL or HostAndPort to be ignored.
+//
+// If you only need to change basic connection options such as providing a password, it is
+// simpler to use DialOptions.
+//
+// Use PoolInterface if you want to provide your own implementation of a connection pool.
+func (b *DataStoreBuilder) Pool(pool *r.Pool) *DataStoreBuilder {
+	b.opts.setPool(pool)
+	return b
+}
+
+// PoolInterface is equivalent to Pool, but uses an interface type rather than a concrete
+// implementation type. This allows implementation of custom behaviors for connection management.
+func (b *DataStoreBuilder) PoolInterface(pool Pool) *DataStoreBuilder {
+	b.opts.setPool(pool)
+	return b
+}
+
+// DialOptions specifies any of the advanced Redis connection options supported by Redigo, such as
+// DialPassword.
+//
+//	import (
+//	    redigo "github.com/gomodule/redigo/redis"
+//	    ldredis "github.com/launchdarkly/go-server-sdk-redis-redigo/v4"
+//	)
+//	config.DataStore = ldcomponents.PersistentDataStore(
+//	    ldredis.DataStore().DialOptions(redigo.DialPassword("verysecure123")),
+//	)
+//
+// Note that some Redis client features can also be specified as part of the URL: see URL.
+func (b *DataStoreBuilder) DialOptions(options ...r.DialOption) *DataStoreBuilder {
+	b.opts.setDialOptions(options)
+	return b
+}
+
+// Build is called internally by the SDK.
+func (b *DataStoreBuilder) Build(context subsystems.ClientContext) (subsystems.PersistentDataStore, error) {
+	return newRedisDataStoreImpl(b.opts, context.GetLogging().Loggers), nil
+}
+
+// DescribeConfiguration is used internally by the SDK to inspect the configuration.
+func (b *DataStoreBuilder) DescribeConfiguration() ldvalue.Value {
+	return ldvalue.String("Redis")
+}
+
+// BigSegmentStoreBuilder is a builder for configuring the Redis-based Big Segment store.
+//
+// Obtain an instance of this type by calling [BigSegmentStore]. After calling its methods to specify
+// any non-default Redis options you may want, pass it to
+// [github.com/launchdarkly/go-server-sdk/v7/ldcomponents.BigSegments] and store the result in the
+// SDK configuration's BigSegments field.
+//
+// To configure the main data store that holds feature flag data, use [DataStore] instead. The two
+// kinds of store are independent of each other, and do not need to use the same database.
+type BigSegmentStoreBuilder struct {
+	opts builderOptions
 }
 
 // BigSegmentStore returns a configurable builder for a Redis-backed Big Segment store.
@@ -63,61 +198,15 @@ func DataStore() *StoreBuilder[subsystems.PersistentDataStore] {
 //	config.BigSegments = ldcomponents.BigSegments(
 //		ldredis.BigSegmentStore().HostAndPort("host2", 6379),
 //	).StatusPollInterval(time.Second * 30)
-func BigSegmentStore() *StoreBuilder[subsystems.BigSegmentStore] {
-	return &StoreBuilder[subsystems.BigSegmentStore]{
-		builderOptions: builderOptions{
-			prefix: DefaultPrefix,
-			url:    DefaultURL,
-		},
-		factory: createBigSegmentStore,
-	}
+func BigSegmentStore() *BigSegmentStoreBuilder {
+	return &BigSegmentStoreBuilder{opts: defaultBuilderOptions()}
 }
 
-// StoreBuilder is a builder for configuring the Redis-based persistent data store and/or Big
-// Segment store.
-//
-// Both [DataStore] and [BigSegmentStore] return instances of this type. You can use methods of the
-// builder to specify any ny non-default Redis options you may want, before passing the builder to
-// either [github.com/launchdarkly/go-server-sdk/v7/ldcomponents.PersistentDataStore] or
-// [github.com/launchdarkly/go-server-sdk/v7/ldcomponents.BigSegments] as appropriate. The two types
-// of stores are independent of each other; you do not need a Big Segment store if you are not using
-// the Big Segments feature, and you do not need to use the same database for both.
-//
-// In this example, the main data store uses a Redis host called "host1", and the Big Segment
-// store uses a Redis host called "host2":
-//
-//     config.DataStore = ldcomponents.PersistentDataStore(
-//         ldredis.DataStore().URL("redis://host1:6379")
-//     config.BigSegments = ldcomponents.BigSegments(
-//         ldredis.DataStore().URL("redis://host2:6379")
-//
-// Note that the SDK also has its own options related to data storage that are configured
-// at a different level, because they are independent of what database is being used. For
-// instance, the builder returned by [github.com/launchdarkly/go-server-sdk/v7/ldcomponents.PersistentDataStore]
-// has options for caching:
-//
-//	config.DataStore = ldcomponents.PersistentDataStore(
-//		ldredis.DataStore().HostAndPort("host1", 6379),
-//	).CacheSeconds(15)
-type StoreBuilder[T any] struct {
-	builderOptions builderOptions
-	factory        func(*StoreBuilder[T], subsystems.ClientContext) (T, error)
-}
-
-type builderOptions struct {
-	prefix      string
-	pool        Pool
-	url         string
-	dialOptions []r.DialOption
-}
-
-// Prefix specifies a string that should be prepended to all Redis keys used by the data store.
-// A colon will be added to this automatically. If this is unspecified or empty, [DefaultPrefix] will be used.
-func (b *StoreBuilder[T]) Prefix(prefix string) *StoreBuilder[T] {
-	if prefix == "" {
-		prefix = DefaultPrefix
-	}
-	b.builderOptions.prefix = prefix
+// Prefix specifies a string that should be prepended to all Redis keys used by the Big Segment
+// store. A colon will be added to this automatically. If this is unspecified or empty,
+// [DefaultPrefix] will be used.
+func (b *BigSegmentStoreBuilder) Prefix(prefix string) *BigSegmentStoreBuilder {
+	b.opts.setPrefix(prefix)
 	return b
 }
 
@@ -127,62 +216,61 @@ func (b *StoreBuilder[T]) Prefix(prefix string) *StoreBuilder[T] {
 // the redis:// syntax (https://www.iana.org/assignments/uri-schemes/prov/redis), which can include a
 // password and a database number, as well as rediss://
 // (https://www.iana.org/assignments/uri-schemes/prov/rediss), which enables TLS.
-func (b *StoreBuilder[T]) URL(url string) *StoreBuilder[T] {
-	if url == "" {
-		url = DefaultURL
-	}
-	b.builderOptions.url = url
+func (b *BigSegmentStoreBuilder) URL(url string) *BigSegmentStoreBuilder {
+	b.opts.setURL(url)
 	return b
 }
 
 // HostAndPort is a shortcut for specifying the Redis host address as a hostname and port.
-func (b *StoreBuilder[T]) HostAndPort(host string, port int) *StoreBuilder[T] {
-	return b.URL(fmt.Sprintf("redis://%s:%d", host, port))
+func (b *BigSegmentStoreBuilder) HostAndPort(host string, port int) *BigSegmentStoreBuilder {
+	b.opts.setHostAndPort(host, port)
+	return b
 }
 
-// Pool specifies that the data store should use a specific connection pool configuration. If not
-// specified, it will create a default configuration (see package description). Specifying this
+// Pool specifies that the Big Segment store should use a specific connection pool configuration. If
+// not specified, it will create a default configuration (see package description). Specifying this
 // option will cause any address specified with URL or HostAndPort to be ignored.
 //
 // If you only need to change basic connection options such as providing a password, it is
 // simpler to use DialOptions.
 //
 // Use PoolInterface if you want to provide your own implementation of a connection pool.
-func (b *StoreBuilder[T]) Pool(pool *r.Pool) *StoreBuilder[T] {
-	b.builderOptions.pool = pool
+func (b *BigSegmentStoreBuilder) Pool(pool *r.Pool) *BigSegmentStoreBuilder {
+	b.opts.setPool(pool)
 	return b
 }
 
 // PoolInterface is equivalent to Pool, but uses an interface type rather than a concrete
 // implementation type. This allows implementation of custom behaviors for connection management.
-func (b *StoreBuilder[T]) PoolInterface(pool Pool) *StoreBuilder[T] {
-	b.builderOptions.pool = pool
+func (b *BigSegmentStoreBuilder) PoolInterface(pool Pool) *BigSegmentStoreBuilder {
+	b.opts.setPool(pool)
 	return b
 }
 
 // DialOptions specifies any of the advanced Redis connection options supported by Redigo, such as
 // DialPassword.
 //
-//     import (
-//         redigo "github.com/garyburd/redigo/redis"
-//         ldredis "github.com/launchdarkly/go-server-sdk-redis-redigo/v4"
-//     )
-//     config.DataSource = ldcomponents.PersistentDataStore(
-//         ldredis.DataStore().DialOptions(redigo.DialPassword("verysecure123")),
-//     )
-// Note that some Redis client features can also be specified as part of the URL: see  URL().
-func (b *StoreBuilder[T]) DialOptions(options ...r.DialOption) *StoreBuilder[T] {
-	b.builderOptions.dialOptions = options
+//	import (
+//	    redigo "github.com/gomodule/redigo/redis"
+//	    ldredis "github.com/launchdarkly/go-server-sdk-redis-redigo/v4"
+//	)
+//	config.BigSegments = ldcomponents.BigSegments(
+//	    ldredis.BigSegmentStore().DialOptions(redigo.DialPassword("verysecure123")),
+//	)
+//
+// Note that some Redis client features can also be specified as part of the URL: see URL.
+func (b *BigSegmentStoreBuilder) DialOptions(options ...r.DialOption) *BigSegmentStoreBuilder {
+	b.opts.setDialOptions(options)
 	return b
 }
 
 // Build is called internally by the SDK.
-func (b *StoreBuilder[T]) Build(context subsystems.ClientContext) (T, error) {
-	return b.factory(b, context)
+func (b *BigSegmentStoreBuilder) Build(context subsystems.ClientContext) (subsystems.BigSegmentStore, error) {
+	return newRedisBigSegmentStoreImpl(b.opts, context.GetLogging().Loggers), nil
 }
 
 // DescribeConfiguration is used internally by the SDK to inspect the configuration.
-func (b *StoreBuilder[T]) DescribeConfiguration() ldvalue.Value {
+func (b *BigSegmentStoreBuilder) DescribeConfiguration() ldvalue.Value {
 	return ldvalue.String("Redis")
 }
 
@@ -190,7 +278,8 @@ func (b *StoreBuilder[T]) DescribeConfiguration() ldvalue.Value {
 //
 // The methods of this interface are the same as the basic methods of the Pool type in
 // the Redigo client. Any type implementing the interface can be passed to
-// StoreBuilder.PoolInterface to provide custom connection behavior.
+// DataStoreBuilder.PoolInterface or BigSegmentStoreBuilder.PoolInterface to provide custom
+// connection behavior.
 type Pool interface {
 	// Get obtains a Redis connection.
 	//
@@ -201,20 +290,4 @@ type Pool interface {
 	//
 	// See: https://pkg.go.dev/github.com/gomodule/redigo/redis#Pool.Close
 	Close() error
-}
-
-func createPersistentDataStore(
-	builder *StoreBuilder[subsystems.PersistentDataStore],
-	clientContext subsystems.ClientContext,
-) (subsystems.PersistentDataStore, error) {
-	store := newRedisDataStoreImpl(builder.builderOptions, clientContext.GetLogging().Loggers)
-	return store, nil
-}
-
-func createBigSegmentStore(
-	builder *StoreBuilder[subsystems.BigSegmentStore],
-	clientContext subsystems.ClientContext,
-) (subsystems.BigSegmentStore, error) {
-	store := newRedisBigSegmentStoreImpl(builder.builderOptions, clientContext.GetLogging().Loggers)
-	return store, nil
 }
--- redis_builder_test.go
+++ redis_builder_test.go
@@ -5,64 +5,106 @@ import (
 
 	r "github.com/gomodule/redigo/redis"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/launchdarkly/go-server-sdk/v7/subsystems"
 )
 
+// The SDK consumes each builder only through the ComponentConfigurer interface for its own store
+// type, so these assertions cover everything the SDK requires of the builder types.
+var (
+	_ subsystems.ComponentConfigurer[subsystems.PersistentDataStore] = (*DataStoreBuilder)(nil)
+	_ subsystems.ComponentConfigurer[subsystems.BigSegmentStore]     = (*BigSegmentStoreBuilder)(nil)
+)
+
+// builderUnderTest adapts one concrete builder type to the shared connection-option assertions.
+// Each field takes and returns the builder type, so assigning a method expression to it also
+// asserts that the option returns its own builder type and that chaining is preserved.
+type builderUnderTest[B any] struct {
+	factory       func() B
+	optionsOf     func(B) builderOptions
+	dialOptions   func(B, ...r.DialOption) B
+	hostAndPort   func(B, string, int) B
+	pool          func(B, *r.Pool) B
+	poolInterface func(B, Pool) B
+	prefix        func(B, string) B
+	url           func(B, string) B
+}
+
 func TestDataStoreBuilder(t *testing.T) {
-	testStoreBuilder(t, DataStore)
+	testConnectionOptions(t, builderUnderTest[*DataStoreBuilder]{
+		factory:       DataStore,
+		optionsOf:     func(b *DataStoreBuilder) builderOptions { return b.opts },
+		dialOptions:   (*DataStoreBuilder).DialOptions,
+		hostAndPort:   (*DataStoreBuilder).HostAndPort,
+		pool:          (*DataStoreBuilder).Pool,
+		poolInterface: (*DataStoreBuilder).PoolInterface,
+		prefix:        (*DataStoreBuilder).Prefix,
+		url:           (*DataStoreBuilder).URL,
+	})
 }
 
 func TestBigSegmentStoreBuilder(t *testing.T) {
-	testStoreBuilder(t, BigSegmentStore)
+	testConnectionOptions(t, builderUnderTest[*BigSegmentStoreBuilder]{
+		factory:       BigSegmentStore,
+		optionsOf:     func(b *BigSegmentStoreBuilder) builderOptions { return b.opts },
+		dialOptions:   (*BigSegmentStoreBuilder).DialOptions,
+		hostAndPort:   (*BigSegmentStoreBuilder).HostAndPort,
+		pool:          (*BigSegmentStoreBuilder).Pool,
+		poolInterface: (*BigSegmentStoreBuilder).PoolInterface,
+		prefix:        (*BigSegmentStoreBuilder).Prefix,
+		url:           (*BigSegmentStoreBuilder).URL,
+	})
 }
 
-func testStoreBuilder[T any](t *testing.T, factory func() *StoreBuilder[T]) {
+func testConnectionOptions[B any](t *testing.T, subject builderUnderTest[B]) {
 	t.Run("defaults", func(t *testing.T) {
-		b := factory()
-		assert.Len(t, b.builderOptions.dialOptions, 0)
-		assert.Nil(t, b.builderOptions.pool)
-		assert.Equal(t, DefaultPrefix, b.builderOptions.prefix)
-		assert.Equal(t, DefaultURL, b.builderOptions.url)
+		opts := subject.optionsOf(subject.factory())
+		assert.Len(t, opts.dialOptions, 0)
+		assert.Nil(t, opts.pool)
+		assert.Equal(t, DefaultPrefix, opts.prefix)
+		assert.Equal(t, DefaultURL, opts.url)
 	})
 
 	t.Run("DialOptions", func(t *testing.T) {
 		o1 := r.DialPassword("p")
 		o2 := r.DialTLSSkipVerify(true)
-		b := factory().DialOptions(o1, o2)
-		assert.Len(t, b.builderOptions.dialOptions, 2) // a DialOption is a function, so can't do an equality test
+		b := subject.dialOptions(subject.factory(), o1, o2)
+		// a DialOption is a function, so can't do an equality test
+		assert.Len(t, subject.optionsOf(b).dialOptions, 2)
 	})
 
 	t.Run("HostAndPort", func(t *testing.T) {
-		b := factory().HostAndPort("mine", 4000)
-		assert.Equal(t, "redis://mine:4000", b.builderOptions.url)
+		b := subject.hostAndPort(subject.factory(), "mine", 4000)
+		assert.Equal(t, "redis://mine:4000", subject.optionsOf(b).url)
 	})
 
 	t.Run("Pool", func(t *testing.T) {
 		p := &r.Pool{MaxActive: 999}
-		b := factory().Pool(p)
-		assert.Equal(t, p, b.builderOptions.pool)
+		b := subject.pool(subject.factory(), p)
+		assert.Equal(t, p, subject.optionsOf(b).pool)
 	})
 
 	t.Run("PoolInterface", func(t *testing.T) {
 		p := &myCustomPool{Pool: r.Pool{MaxActive: 999}}
-		b := factory().PoolInterface(p)
-		assert.Equal(t, p, b.builderOptions.pool)
+		b := subject.poolInterface(subject.factory(), p)
+		assert.Equal(t, p, subject.optionsOf(b).pool)
 	})
 
 	t.Run("Prefix", func(t *testing.T) {
-		b := factory().Prefix("p")
-		assert.Equal(t, "p", b.builderOptions.prefix)
+		b := subject.prefix(subject.factory(), "p")
+		assert.Equal(t, "p", subject.optionsOf(b).prefix)
 
-		b.Prefix("")
-		assert.Equal(t, DefaultPrefix, b.builderOptions.prefix)
+		b = subject.prefix(b, "")
+		assert.Equal(t, DefaultPrefix, subject.optionsOf(b).prefix)
 	})
 
 	t.Run("URL", func(t *testing.T) {
 		url := "redis://mine"
-		b := factory().URL(url)
-		assert.Equal(t, url, b.builderOptions.url)
+		b := subject.url(subject.factory(), url)
+		assert.Equal(t, url, subject.optionsOf(b).url)
 
-		b.URL("")
-		assert.Equal(t, DefaultURL, b.builderOptions.url)
+		b = subject.url(b, "")
+		assert.Equal(t, DefaultURL, subject.optionsOf(b).url)
 	})
 }
 
